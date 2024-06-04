4fbd13eb80ace218caf3f9c6d7cbe993bd2be672
mdehfgit <mde@hellofresh.com>
2024-06-04 15:15:40 +0200

    minor clean up

--- index.js
+++ index.js
@@ -3,31 +3,31 @@ const apiKey = 'a9153c42-41e6-416b-b597-7e86b6a8c25a';
 
 
 
-document.getElementById("submit").addEventListener("click", function() {
-  const inputText = document.getElementById("inputBox").value;
+document.getElementById("submit").addEventListener("click", function () {
+    const inputText = document.getElementById("inputBox").value;
 
-  const data = {
-    q: [inputText],
-    source: "en",
-    target: "de",
-    format: "text",
-    api_key: apiKey
-  };
+    const data = {
+        q: inputText,
+        source: "en",
+        target: "de",
+        format: "text",
+        api_key: apiKey
+    };
 
-  const typeOfRequest = {
-    method: "POST",
-    headers: {
-        'Accept': "application/json",
-        'Content-Type': "application/json" 
-    },
-    body: JSON.stringify(data)
-  };
+    const typeOfRequest = {
+        method: "POST",
+        headers: {
+            'Accept': "application/json",
+            'Content-Type': "application/json"
+        },
+        body: JSON.stringify(data)
+    };
 
-  fetch( apiUrl, typeOfRequest)
-    .then(response => response.json())
-    .then(data => { 
-    console.log (data)
-      const translatedText = data.translatedText
-      document.getElementById("outputBox").innerText = translatedText;
-    });
+    fetch(apiUrl, typeOfRequest)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            const translatedText = data.translatedText
+            document.getElementById("outputBox").innerText = translatedText;
+        });
 });
