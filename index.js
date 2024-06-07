2f6f361e04f2a647c75109d225be11d4da5332ad
Mohan <mde@hellofresh.com>
2024-06-07 11:51:30 +0200

    added spinner

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ const apiKey = 'a9153c42-41e6-416b-b597-7e86b6a8c25a';
 
 
 document.getElementById("submit").addEventListener("click", function () {
+    const spinner = document.getElementById('spinner');
     const inputText = document.getElementById("inputBox").value;
     const source = document.getElementById("fromLang").value;
     const target = document.getElementById("toLang").value;
+
+    spinner.style.display = 'block';
 
     const data = {
         q: inputText,
@@ -27,10 +30,12 @@ document.getElementById("submit").addEventListener("click", function () {
 
     fetch(apiUrl, typeOfRequest)
         .then(response => response.json())
-        .then(data => {
+        .then (data=> {
+            spinner.style.display = 'none';
             const translatedText = data.translatedText
             document.getElementById("outputBox").value = translatedText;
         });
+      
 });
 
 document.getElementById("clear").addEventListener("click", function () {
